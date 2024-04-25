8f77a5e1895659dae4f0b91fbb150afc4a1f4095
Vishnu1173 <vishnurandy1173@gmail.com>
2024-04-25 12:25:41 +0530

    FileMethods

---
+++ FileMethod/FileClass.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace FileMethod
+{
+    public class FileClass
+    {
+        public bool StoreData(string path,string content)
+        {
+            File.WriteAllText(path, content);
+            return true; 
+        }    
+    }
+}
---
+++ FileMethod/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.IO;
+
+namespace FileMethod
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+         
+        private void btnsave_Click(object sender, RoutedEventArgs e)
+        {
+            //File.Create(@"D:\Demo\Sathish.Txt"); ( Just create File )
+            //File.WriteAllText(@"D:\Demo\Sathish.doc", "welcome"); (  With Store SomeDatas and next statement same and method used with bool true or false)
+            FileClass fileClass =new FileClass();
+            bool isSuccess = fileClass.StoreData(@"D:\Demo\Sathish.txt", "Hi");
+            if(isSuccess)
+            {
+                MessageBox.Show("Data Saved");
+            }
+        }
+    }
+}
